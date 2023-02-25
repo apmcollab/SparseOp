0d2d89f1515b505cf311e1f261cbe3397a76ba2a
Chris Anderson <anderson@math.ucla.edu>
2023-02-24 16:13:40 -0800

    added gnu license statement

--- SCC_IndexMap3d.h
+++ SCC_IndexMap3d.h
@@ -4,7 +4,26 @@
  *  Created on: Jan 15, 2023
  *      Author: anderson
  */
-
+/*
+#############################################################################
+#
+# Copyright  2023  Chris Anderson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# For a copy of the GNU General Public License see
+# <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+*/
 #ifndef INDEX_MAP_3D_
 #define INDEX_MAP_3D_
 
